073e65a98a8579cadfd08329bbf08f1ec0b70cb9
Jenna Goldberg <jg346015@ohio.edu>
2019-10-09 10:26:23 -0400

    Profile Creation

---
+++ MIS4200team3/CentricTeam3Context/DAL/Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MIS4200team3.Models;
+using System.Data.Entity;
+
+namespace MIS4200team3.CentricTeam3Context.DAL
+{
+    public class Context : DbContext
+    {
+        public Context() : base ("name=DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Profile> Profile { get; set; }
+    }
+}
---
+++ MIS4200team3/Models/Profile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MIS4200team3.Models
+{
+    public class Profile
+    {
+        public int profileID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string businessUnit { get; set; }
+        public string hireDate { get; set; }
+        public string employeeTitle { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+    }
+}
