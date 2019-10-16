2557b52fead1e4b2bbcfd57a3fc3e6dcc0504e6e
Hannah Wilson <hw601315@ohio.edu>
2019-10-16 10:34:25 -0400

    Created recognition model

---
+++ MIS4200team3/Models/Recognition.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MIS4200team3.Models
+{
+    public class Recognition
+    {
+        public int recognitionID { get; set;}
+
+        public string description { get; set; }
+
+        public cValues values { get; set; }
+
+        public enum cValues
+        {
+            Stewardship,
+            Culture,
+            DeliveryExcellance,
+            Innovation,
+            GreaterGood,
+            Integrity,
+            Balance
+        }
+
+        
+        public int profileID { get; set; }
+        public virtual Profile Profile { get; set; }
+
+
+
+
+
+    }
+}
