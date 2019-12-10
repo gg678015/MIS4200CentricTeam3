885de2615a04aa6027fb886be87b283c7e9cb51b
Georgia Golec <gg678015@ohio.edu>
2019-12-10 15:55:26 -0500

    works

--- MIS4200team3/Controllers/RecognitionsController.cs
+++ MIS4200team3/Controllers/RecognitionsController.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using MIS4200team3.DAL;
 using MIS4200team3.Models;
-using System.Net;
 using System.Net.Mail;
 using Microsoft.AspNet.Identity;
 
